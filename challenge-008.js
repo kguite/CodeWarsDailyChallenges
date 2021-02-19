4cc9aaab2e98a2003409b396bc3e44b424ed0623
kguite <kguite@gmail.com>
2021-02-19 11:09:00 -0800

    8kyu Sum of Positive

--- challenge-008.js
+++ challenge-008.js
@@ -1,11 +1,11 @@
 //8kyu Sum of Positive
 
 function positiveSum(arr) {
-	  let sum = 0;
-	  for(i = 0; i <arr.length; i++) {
-		if(arr[i] > 0) {
-		  sum += arr[i];
-		}
-	  }
-	  return sum
+	let sum = 0;
+	for(i = 0; i <arr.length; i++) {
+	if(arr[i] > 0) {
+	sum += arr[i];
 	}
+}
+	return sum
+}
