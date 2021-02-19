1eadac5839716d6548e675708880a40ecfac23d6
kguite <kguite@gmail.com>
2021-02-19 11:02:58 -0800

    8kyu Find the Smallest integer in array

--- challenge-013.js
+++ challenge-013.js
@@ -4,4 +4,4 @@
 
 function repeatStr (n, s) {
 	  return s.repeat(n);
-	}
+}
---
+++ challenge-014.js
@@ -0,0 +1,19 @@
+// 8kyu Find the Smallest integer in array
+// had to look at solution
+// confused on what to do with the class? and don't see an array at all in the provided starter code:
+
+class SmallestIntegerFinder {
+	  findSmallestInt(args) {
+	  
+	  }
+	}
+	
+// solution is:
+
+class SmallestIntegerFinder {
+	  findSmallestInt(args) {
+		return Math.min(...args)
+	  }
+	}
+	
+// I tried Math.min, but did not add 'return or the (...)
