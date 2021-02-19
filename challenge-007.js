fa3e8d0c048fec7ec247bfcad906baa6e4cd58ed
kguite <kguite@gmail.com>
2021-02-19 11:10:44 -0800

    8kyu Square(n) Sum

--- challenge-007.js
+++ challenge-007.js
@@ -1,10 +1,10 @@
 //8kyu Square(n) Sum
 
 function squareSum(numbers){
-	  let total = 0,
-		square = numbers.map(item => item * item);
-	  for (let i = 0; i < square.length; i++) {
-		total += square[i];
-	  }
-	  return total;
+	let total = 0,
+	square = numbers.map(item => item * item);
+	for (let i = 0; i < square.length; i++) {
+	total += square[i];
 	}
+	return total;
+}
