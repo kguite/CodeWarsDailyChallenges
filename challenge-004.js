f42c980dc001325dd04e7bcb336057712bf74531
kguite <kguite@gmail.com>
2021-02-19 11:13:36 -0800

    8kyu Remove First and Last Character

--- challenge-004.js
+++ challenge-004.js
@@ -1,9 +1,9 @@
 //8ky Remove First and Last Character
 
 function removeChar(str) {
-	  const array = str.split("");
-	  let res = "";
-	  for (let i = 1; i < array.length - 1; i++) res += array[i];
-	  return res;
-	}
+	const array = str.split("");
+	let res = "";
+	for (let i = 1; i < array.length - 1; i++) res += array[i];
+	return res;
+}
 	
