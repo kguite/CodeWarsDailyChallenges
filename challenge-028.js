b9fa4e1fb9732a6e617f2c35a6395fc4d6ed7a93
kguite <kguite@gmail.com>
2021-02-20 21:46:15 -0800

    8kyu Simple Fun #1: Seats in Theater

---
+++ challenge-028.js
@@ -0,0 +1,8 @@
+// 8kyu Simple Fun #1: Seats in Theater
+
+function seatsInTheater(nCols, nRows, col, row) {
+	  //coding and coding..
+	return (nCols - col + 1) * (nRows - row) //total number of seats in theater, 176, minus the columns the person is in, plus the column they're actually in)
+	
+	  
+	}
