0b01d9e7fef971545a4d971098e1229c70cd30d5
kguite <kguite@gmail.com>
2021-02-23 17:51:04 -0800

    Pokemon Array Reverse

---
+++ challenge-034.js
@@ -0,0 +1,8 @@
+//Arrays
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+
+function reverseParty(arr){
+	return arr.reverse()
+}
+	
+console.log(reverseParty(['bulba', 'pika', 'butter']))
