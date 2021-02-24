4e68647ee6fbc612a0ebf7d3debbf93ea961ec54
kguite <kguite@gmail.com>
2021-02-23 17:05:53 -0800

    Pokemon Variables

---
+++ challenge-031.js
@@ -0,0 +1,14 @@
+// no KYU, from Leon not CodeWars
+
+// Variables
+//You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
+
+let bulbasourCandies = 11
+let caterpieCandies = 6
+let weedleCandies = 6
+
+let totalCandies = bulbasourCandies + caterpieCandies + weedleCandies
+
+console.log(totalCandies)
+
+
