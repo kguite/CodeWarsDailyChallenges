00047e3cd2e0ca6cbd6035b70749a9d4a37e0db1
kguite <kguite@gmail.com>
2021-02-23 17:21:38 -0800

    Pokemon League Loops

---
+++ challenge-033.js
@@ -0,0 +1,12 @@
+//Loops
+//You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
+
+//creating a function that takes in a Number
+// prints something x times
+
+function underPokemonLeague(numOfPokemon){
+	for(let i = 0; i < numOfPokemon; i++)
+	console.log("Pikachu, I choose you!")
+}
+
+underPokemonLeague(10)
