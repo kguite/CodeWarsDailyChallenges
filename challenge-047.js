fd0b27bdc77ac8953afaef4ba69a29183c4fc862
kguite <kguite@gmail.com>
2021-03-07 15:06:48 -0800

    find first nonconsecutive in array

---
+++ challenge-047.js
@@ -0,0 +1,8 @@
+//8kyu First NonConsecutive
+
+function firstNonConsecutive (arr) {
+	  const result = arr.find((number, index) => number !== index + arr[0])
+	  return Number.isInteger(result) ? result : null
+	}
+	
+	
