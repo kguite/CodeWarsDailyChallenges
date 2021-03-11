587caa19e33e093aa0ee046d1163b7988ef5b171
kguite <kguite@gmail.com>
2021-03-11 10:33:14 -0800

    finding maximum and minimum values of a list

---
+++ challenge-048.js
@@ -0,0 +1,6 @@
+// find Maximum and Minimum Values of a List
+
+// trying to use arrow functions and minimalism.  LIKE this one!
+
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
