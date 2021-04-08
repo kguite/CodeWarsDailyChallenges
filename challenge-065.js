6c92548bf2f4f5b390c712c21c72ceaa5041a44e
kguite <kguite@gmail.com>
2021-04-07 23:03:39 -0700

    logical calculator

---
+++ challenge-065.js
@@ -0,0 +1,9 @@
+//8kyu logical calculator
+
+const operations = {
+	  AND: (a, b) => a && b,
+	  OR: (a, b) => a || b,
+	  XOR: (a, b) => a !== b,
+	}
+	
+	const logicalCalc = (array, op) => array.reduce(operations[op])
