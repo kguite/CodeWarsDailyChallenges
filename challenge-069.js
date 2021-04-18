6c1afeefe28fc6f91a09a822eb8eb4c0c6909ae3
kguite <kguite@gmail.com>
2021-04-17 20:25:19 -0700

    8kyu Keep up the Hoop

---
+++ challenge-069.js
@@ -0,0 +1,11 @@
+//8kyu Keep up the Hoop
+
+function hoopCount (n) {
+	   if (n >= 10){
+		  return "Great, now move on to tricks"
+	 } else {
+	   return "Keep at it until you get it"
+	 }
+	}
+	
+	
