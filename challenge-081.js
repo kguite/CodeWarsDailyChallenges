a3c0e6f2a02b591b742bf95e8c84f30f147ea70f
kguite <kguite@gmail.com>
2021-06-01 22:07:51 -0700

    8kyu BMI (if statement, Math.pow(height, 2), ternary)

--- challenge-081.js
+++ challenge-081.js
@@ -2,7 +2,26 @@
 
 // getting brave with some ternaries!
 
+// forgot about Math.pow - DOH.
+
 const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
 bmi <= 25 ? "Normal" :
 bmi <= 30 ? "Overweight" : "Obese";
 
+// forgot about Math.pow - DOH. Another simple, easy to read solution:
+
+function bmi(weight, height) {
+	  var result = weight/Math.pow(height,2) 
+	  
+	  if (result <= 18.5) {
+		return "Underweight";
+	  } else if (result <= 25) {
+		return "Normal";
+	  } else if (result <= 30) {
+		return "Overweight";
+	  } else {
+		return "Obese";
+	  }
+	  
+	}
+	
