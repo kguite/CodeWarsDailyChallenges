31c57c09461d1fc3b82ee7240b2845bf1d521405
kguite <kguite@gmail.com>
2021-06-02 21:35:41 -0700

    8kyu Area or Perimeter (if statement)

---
+++ challenge-084.js
@@ -0,0 +1,18 @@
+//8kyu Area or Perimeter
+
+
+const areaOrPerimeter = function(l , w) {
+	  if (l === w) {
+		return l*w
+	  } else {
+		return 2 * (l+w)
+	  }
+	};
+	
+	// OR
+	
+	
+const areaOrPerimeter = function(l , w) {
+	  if (l===w) return l*w
+	  return 2 * (l+w)
+	};
