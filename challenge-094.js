34d2e81f3e6cc044933fcee1ae3c04cbdcd3e2e4
kguite <kguite@gmail.com>
2021-07-05 16:46:03 -0700

    8kyu Stringy String .repeat, .slice

--- challenge-094.js
+++ challenge-094.js
@@ -3,3 +3,9 @@
 const stringy = size => "10".repeat(size).slice(0,size);
 
 
+//PREP
+//Parameters: a string of any positive length
+// Return: a string of "10" repeated as many times as size is specified,
+// Example:  so if size = 2, the output is "10".  If size = 6, the output is "101010".
+
+// slice method extracts a section of a string and returns it as a new string, without modifying the original string.  Above, .slice is supplied with the beginningIndex(0 and the end/length (size)
